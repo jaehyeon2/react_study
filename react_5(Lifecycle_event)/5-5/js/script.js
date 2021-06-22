fe94bf9ce645efd046083a3b5f9f9a1f14bf6283
강재현 <wol5971@gmail.com>
2021-06-22 10:25:15 +0000

    commit

--- react_5(Lifecycle_event)/5-5/js/script.js
+++ react_5(Lifecycle_event)/5-5/js/script.js
@@ -1,4 +1,4 @@
-let dataUrl = '/react_5\(Lifecycle_event\)/5-5/real-user-data.json';
+let dataUrl = 'real-user-data.json';
 ReactDOM.render(React.createElement(
 	'div',
 	null,
--- react_5(Lifecycle_event)/5-5/jsx/script.jsx
+++ react_5(Lifecycle_event)/5-5/jsx/script.jsx
@@ -1,4 +1,4 @@
-let dataUrl = '/react_5\(Lifecycle_event\)/5-5/real-user-data.json'
+let dataUrl = 'real-user-data.json'
 ReactDOM.render(
 	<div>
 		<Users data-url={dataUrl}/>
